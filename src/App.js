80e210c20d3fe002142d005753d5c5bd93c36655
User Name <artdevsemyonov@gmail.com>
2022-12-13 09:25:28 +0300

    Add react-router-dom and imported component Route for page navigation & Link instead of tags a

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import Container from "@mui/material/Container";
+
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+
+          {/*<AddPost />*/}
+          {/*<Login />*/}
+          {/*<Registration />*/}
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
+
+export const Header = () => {
+  const isAuth = false;
+
+  const onClickLogout = () => {};
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>LOGO</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/posts/create">
+                  <Button variant="contained">Написать статью</Button>
+                </Link>
+                <Button onClick={onClickLogout} variant="contained" color="error">
+                  Выйти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Войти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
