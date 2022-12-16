ada6ddcf59dc65cedb35f03e6f5f81e2cc06c68a
User Name <artdevsemyonov@gmail.com>
2022-12-16 15:48:00 +0300

    Добавил корректный вывод статей с бэкенда и убрал ошибку корса

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const { data } = await axios.get('/posts');
+    return data;
+});
 
 const initialState = {
     posts: {
@@ -14,7 +20,21 @@ const initialState = {
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducer: {},
+    reducers: {},
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'loading';
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.posts.items = action.payload;
+            state.posts.status = 'loaded';
+        },
+        [fetchPosts.rejected]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'error';
+        },
+    },
 });
 
 export const postsReducer = postsSlice.reducer;
