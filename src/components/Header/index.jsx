7767b4aa32f10ddeac9d6f8fd8512125841e6f00
User Name <artdevsemyonov@gmail.com>
2022-12-20 05:25:49 +0300

    Добавил авторизацию через бэкенд с выводом на фронтенд и плюс корректное отображение и доступ к любым функциям зарегистрированного пользователя из любой точки сайта используя токен(на фронте) и промежуточную функцию на бэке

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,19 @@ import Button from '@mui/material/Button';
 
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
+import { logout, selectIsAuth } from "../../redux/slices/auth";
+import { useDispatch, useSelector } from 'react-redux';
 
 export const Header = () => {
-  const isAuth = false;
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if (window.confirm('Вы действительно хотите выйти ?')) {
+      dispatch(logout());
+      window.localStorage.removeItem('token');
+    }
+  };
 
   return (
     <div className={styles.root}>
